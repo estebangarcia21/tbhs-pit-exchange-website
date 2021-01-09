157f55423d883d1cd2deed1564c0521d0e62b7c1
Esteban Garcia <estebangarcia2121@gmail.com>
2021-01-08 20:11:05 -0800

    Centered auction text

--- styles/pages/index.ts
+++ styles/pages/index.ts
@@ -2,9 +2,15 @@ import { composeClassName } from "@App/utils/tailwind-css";
 
 const header = composeClassName("font-bold text-5xl");
 const text = composeClassName("text-gray-600");
+const navButton = composeClassName(
+    "px-5 p-2 border border-white rounded-sm text-white mx-3 transition hover:bg-white cursor-pointer hover:text-black"
+);
 
 export const useIndexStyles = () => ({
     header: header(),
     text: text("text-lg"),
-    auctionText: text("text-base md:wrap-text-30 md:text-left md:ml-12"),
+    auctionText: text(
+        "text-base text-center md:wrap-text-30 md:text-left md:ml-12"
+    ),
+    navButton: navButton(),
 });
