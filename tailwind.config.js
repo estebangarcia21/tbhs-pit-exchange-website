f63baf2dc35cdbc628f393617d0b6ac452cef873
Esteban Garcia <estebangarcia2121@gmail.com>
2021-01-09 19:06:24 -0800

    Added light blue and cyan theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-    purge: ["./pages/**/*.tsx", "./components/**/*.tsx"],
+    purge: ["./pages/**/*.tsx", "./components/**/*.tsx", "./styles/**/*.tsx"],
     darkMode: false,
-    theme: {},
-    variants: {
-        extend: {},
+    theme: {
+        minWidth: {
+            375: "375px",
+        },
+        colors: {
+            ...colors,
+            "light-blue": "#0ea5e9",
+            cyan: "#22d3ee",
+            "cyan-500": "#06b6d4",
+        },
     },
-    plugins: [],
 };
