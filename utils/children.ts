5193152732de19a6e0585cc86d1ba2ab35791e09
Esteban Garcia <estebangarcia2121@gmail.com>
2021-01-09 19:06:00 -0800

    Added withChildren hoc

--- utils/children.ts
+++ utils/children.ts
@@ -1,3 +1,7 @@
 export interface Children {
     children: React.ReactNode;
 }
+
+export const withChildren = <T extends unknown = {}>(
+    component: (props: T & Children) => JSX.Element
+) => component;
