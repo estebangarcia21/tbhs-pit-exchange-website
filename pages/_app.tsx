679e3f70c3c03c7433390acdd8f2d50dda091a14
Esteban Garcia <estebangarcia2121@gmail.com>
2021-01-09 19:04:29 -0800

    Removed extra semicolon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ const queryClient = new QueryClient();
 function MyApp({ Component, pageProps }) {
     return (
         <QueryClientProvider client={queryClient}>
-            <Component {...pageProps} />;
+            <Component {...pageProps} />
         </QueryClientProvider>
     );
 }
