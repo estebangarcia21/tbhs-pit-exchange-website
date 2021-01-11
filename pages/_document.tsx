325ded045af28531f1396d2787d6df002caff250
Esteban Garcia <estebangarcia2121@gmail.com>
2021-01-10 20:50:51 -0800

    Added theme checking

---
+++ pages/_document.tsx
@@ -0,0 +1,42 @@
+import Document, {
+    DocumentContext,
+    Head,
+    Html,
+    Main,
+    NextScript,
+} from "next/document";
+
+const AppDocument = ({ data }) => {
+    return (
+        <Html lang="en" className={data.isDark ? "dark" : undefined}>
+            <Head>
+                <link rel="shortcut icon" href="/icons/wing.svg" />
+            </Head>
+
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
+};
+
+AppDocument.getInitialProps = async (ctx: DocumentContext) => {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return {
+        ...initialProps,
+        data: {
+            isDark: await getUserTheme(),
+        },
+    };
+};
+
+AppDocument.renderDocument = Document.renderDocument;
+
+const getUserTheme = async () => false;
+
+// const getUserTheme = async () =>
+//     await request<UserAmountQuery>(API_URL, UserAmountDocument);
+
+export default AppDocument;
