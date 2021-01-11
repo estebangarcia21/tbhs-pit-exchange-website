7709adb277e83cf535548450b739d5095b8b0ad8
Esteban Garcia <estebangarcia2121@gmail.com>
2021-01-10 20:48:56 -0800

    Added multiple nav links

--- components/pages/index/Navbar.tsx
+++ components/pages/index/Navbar.tsx
@@ -11,7 +11,7 @@ const NavbarButton = withChildren<NavbarButtonProps>(
         const normalStyles =
             "border-gray-600 bg-white text-black hover:bg-black hover:text-white";
         const invertedStyles =
-            "border-black bg-black text-white hover:bg-white hover:text-black";
+            "border-black bg-black text-white hover:bg-gray-900";
 
         return (
             <Link href={href}>
@@ -31,18 +31,29 @@ const NavbarItem = withChildren(({ children }) => {
     return <li>{children}</li>;
 });
 
+const Container = withChildren(({ children }) => {
+    return <div className="flex flex-row space-x-5">{children}</div>;
+});
+
 export const Navbar = () => {
     return (
-        <nav className="border-b h-16 border-gray-200">
-            <ul className="flex flex-row flex-wrap content-center justify-end h-full mx-5 space-x-5">
-                <NavbarItem>
-                    <NavbarButton href="/login">Log In</NavbarButton>
-                </NavbarItem>
-                <NavbarItem>
-                    <NavbarButton href="/signup" inverted>
-                        Sign Up
-                    </NavbarButton>
-                </NavbarItem>
+        <nav className="border-b h-16 bg-white border-gray-200">
+            <ul className="flex flex-row flex-wrap content-center justify-between h-full mx-5 space-x-5">
+                <Container>
+                    {/* <NavbarItem>Mystic Searcher</NavbarItem>
+                    <NavbarItem>Hey</NavbarItem> */}
+                </Container>
+
+                <Container>
+                    <NavbarItem>
+                        <NavbarButton href="/login">Log In</NavbarButton>
+                    </NavbarItem>
+                    <NavbarItem>
+                        <NavbarButton href="/signup" inverted>
+                            Sign Up
+                        </NavbarButton>
+                    </NavbarItem>
+                </Container>
             </ul>
         </nav>
     );
