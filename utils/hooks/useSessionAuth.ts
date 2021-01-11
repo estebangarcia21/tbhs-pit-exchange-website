138b5f7b31716381047933e45e9a39e9ef3bfd14
Esteban Garcia <estebangarcia2121@gmail.com>
2021-01-10 20:50:57 -0800

    Added session auth hook

---
+++ utils/hooks/useSessionAuth.ts
@@ -0,0 +1,27 @@
+import {
+    MeDocument,
+    MeQuery,
+    MeQueryVariables,
+} from "@App/generated/graphql-types";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useQuery } from "react-query";
+import { serverClient } from "../server-client";
+
+export const useSessionAuth = () => {
+    const router = useRouter();
+
+    const { data, isLoading } = useQuery(
+        "me",
+        async () =>
+            await serverClient.request<MeQuery, MeQueryVariables>(MeDocument)
+    );
+
+    useEffect(() => {
+        if (!isLoading && !data?.me) {
+            router.replace("/login");
+        }
+    }, [isLoading, data, router]);
+
+    return { isLoading: isLoading && !data };
+};
