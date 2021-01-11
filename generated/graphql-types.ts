19066f6f137f61dbf87ccda2a47e5e5692bfec2d
Esteban Garcia <estebangarcia2121@gmail.com>
2021-01-10 20:49:39 -0800

    Updated types

--- generated/graphql-types.ts
+++ generated/graphql-types.ts
@@ -12,10 +12,12 @@ export type Scalars = {
   Boolean: boolean;
   Int: number;
   Float: number;
+  /** The javascript `Date` as string. Type represents date and time as the ISO Date string. */
+  DateTime: any;
 };
 
 export type Query = {
-  __typename?: 'Query';
+  scammers: Array<Scammer>;
   me?: Maybe<User>;
   userAmount: Scalars['Int'];
   getUserByUuid: User;
@@ -26,8 +28,17 @@ export type QueryGetUserByUuidArgs = {
   uuid: Scalars['String'];
 };
 
+export type Scammer = {
+  id: Scalars['Int'];
+  uuid: Scalars['String'];
+  createdAt: Scalars['DateTime'];
+  discordId: Scalars['String'];
+  evidence?: Maybe<Scalars['String']>;
+  reason: Scalars['String'];
+};
+
+
 export type User = {
-  __typename?: 'User';
   id: Scalars['Int'];
   email: Scalars['String'];
   password: Scalars['String'];
@@ -35,18 +46,17 @@ export type User = {
 };
 
 export type MinecraftAccount = {
-  __typename?: 'MinecraftAccount';
   uuid: Scalars['String'];
   username: Scalars['String'];
   verificationToken: Scalars['String'];
 };
 
 export type Mutation = {
-  __typename?: 'Mutation';
   createTrade: Trade;
   login: AuthResponse;
   logout: Scalars['Boolean'];
   registerUser: AuthResponse;
+  resetPassword: ResetPasswordResponse;
 };
 
 
@@ -64,8 +74,12 @@ export type MutationRegisterUserArgs = {
   data: AuthArgs;
 };
 
+
+export type MutationResetPasswordArgs = {
+  email: Scalars['String'];
+};
+
 export type Trade = {
-  __typename?: 'Trade';
   id: Scalars['Int'];
   user: User;
   endUser: User;
@@ -78,15 +92,14 @@ export type CreateTradeInput = {
 };
 
 export type AuthResponse = {
-  __typename?: 'AuthResponse';
+  successful: Scalars['Boolean'];
   user?: Maybe<User>;
-  error?: Maybe<Error>;
+  error?: Maybe<AuthorizationError>;
 };
 
-export type Error = {
-  __typename?: 'Error';
+export type AuthorizationError = {
   object: Scalars['String'];
-  description?: Maybe<Scalars['String']>;
+  message?: Maybe<Scalars['String']>;
 };
 
 export type AuthArgs = {
@@ -94,40 +107,63 @@ export type AuthArgs = {
   password: Scalars['String'];
 };
 
+export type ResetPasswordResponse = {
+  successful: Scalars['Boolean'];
+  errorMessage?: Maybe<Scalars['String']>;
+};
+
+export type LoginMutationVariables = Exact<{
+  email: Scalars['String'];
+  password: Scalars['String'];
+}>;
+
+
+export type LoginMutation = { login: (
+    Pick<AuthResponse, 'successful'>
+    & { error?: Maybe<Pick<AuthorizationError, 'object' | 'message'>> }
+  ) };
+
+export type ResetPasswordMutationVariables = Exact<{
+  email: Scalars['String'];
+}>;
+
+
+export type ResetPasswordMutation = { resetPassword: Pick<ResetPasswordResponse, 'successful' | 'errorMessage'> };
+
 export type MeQueryVariables = Exact<{ [key: string]: never; }>;
 
 
-export type MeQuery = (
-  { __typename?: 'Query' }
-  & { me?: Maybe<(
-    { __typename?: 'User' }
-    & Pick<User, 'id' | 'email' | 'password'>
-    & { minecraftAccount?: Maybe<(
-      { __typename?: 'MinecraftAccount' }
-      & Pick<MinecraftAccount, 'uuid' | 'verificationToken'>
-    )> }
-  )> }
-);
+export type MeQuery = { me?: Maybe<Pick<User, 'id'>> };
 
 export type UserAmountQueryVariables = Exact<{ [key: string]: never; }>;
 
 
-export type UserAmountQuery = (
-  { __typename?: 'Query' }
-  & Pick<Query, 'userAmount'>
-);
+export type UserAmountQuery = Pick<Query, 'userAmount'>;
 
 
+export const LoginDocument = gql`
+    mutation Login($email: String!, $password: String!) {
+  login(data: {email: $email, password: $password}) {
+    successful
+    error {
+      object
+      message
+    }
+  }
+}
+    `;
+export const ResetPasswordDocument = gql`
+    mutation ResetPassword($email: String!) {
+  resetPassword(email: $email) {
+    successful
+    errorMessage
+  }
+}
+    `;
 export const MeDocument = gql`
     query Me {
   me {
     id
-    email
-    password
-    minecraftAccount {
-      uuid
-      verificationToken
-    }
   }
 }
     `;
@@ -143,6 +179,12 @@ export type SdkFunctionWrapper = <T>(action: () => Promise<T>) => Promise<T>;
 const defaultWrapper: SdkFunctionWrapper = sdkFunction => sdkFunction();
 export function getSdk(client: GraphQLClient, withWrapper: SdkFunctionWrapper = defaultWrapper) {
   return {
+    Login(variables: LoginMutationVariables, requestHeaders?: Headers): Promise<LoginMutation> {
+      return withWrapper(() => client.request<LoginMutation>(print(LoginDocument), variables, requestHeaders));
+    },
+    ResetPassword(variables: ResetPasswordMutationVariables, requestHeaders?: Headers): Promise<ResetPasswordMutation> {
+      return withWrapper(() => client.request<ResetPasswordMutation>(print(ResetPasswordDocument), variables, requestHeaders));
+    },
     Me(variables?: MeQueryVariables, requestHeaders?: Headers): Promise<MeQuery> {
       return withWrapper(() => client.request<MeQuery>(print(MeDocument), variables, requestHeaders));
     },
