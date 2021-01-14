7c0ef8a364f2d051204c9515a6206dc17259369f
Esteban Garcia <estebangarcia2121@gmail.com>
2021-01-14 09:35:41 -0800

    withPWA only in production

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 const withPWA = require("next-pwa");
 
-module.exports = process.env.NODE_ENV === "production" ? withPWA() : {};
+module.exports = withPWA({
+    pwa: {
+        disable: process.env.NODE_ENV === "development",
+        register: true,
+        scope: "/app",
+        sw: "service-worker.js",
+    },
+});
