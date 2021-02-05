dfcc9732dd1e5690cf75594ea4a910e94b445b8c
Esteban Garcia <estebangarcia2121@gmail.com>
2021-02-05 15:06:28 -0800

    Added default SEO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,15 @@
-import "styles/index.css";
+import { DefaultSeo } from "next-seo";
 import { QueryClient, QueryClientProvider } from "react-query";
+import "styles/index.css";
+import SEO from "../next-seo.config";
 
 const queryClient = new QueryClient();
 
-function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
     return (
         <QueryClientProvider client={queryClient}>
+            <DefaultSeo {...SEO} />
             <Component {...pageProps} />
         </QueryClientProvider>
     );
 }
-
-export default MyApp;
