db125732bd27796e29f0914974d8c610e76459de
Esteban Garcia <estebangarcia2121@gmail.com>
2021-02-07 12:38:33 -0800

    Removed unused Layout components

--- components/global/Layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Children } from "utils/children";
-
-export const Content = ({ children }: Children) => {
-    return <div className="flex flex-col min-h-screen">{children}</div>;
-};
-
-export const MainContent = ({ children }: Children) => {
-    return <main className="flex-grow">{children}</main>;
-};
