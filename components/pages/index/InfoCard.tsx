891b39ff52a8ccccf487a00c432cd02fc5b87455
Esteban Garcia <estebangarcia2121@gmail.com>
2021-02-07 12:02:06 -0800

    Changed breakpoints to lg

--- components/pages/index/InfoCard.tsx
+++ components/pages/index/InfoCard.tsx
@@ -8,7 +8,7 @@ interface InfoCard extends Children {
 
 export const InfoCard = ({ icon: Icon, title, children }: InfoCard) => {
     return (
-        <div className="sm:flex-grow-0 sm:w-80 py-5 flex-grow">
+        <div className="flex-grow md:m-5 xl:m-0 sm:flex-grow-0 sm:w-80 py-5">
             <Icon className="text-3xl text-rose-500" />
 
             <h2 className="my-1 tracking-wide font-medium text-2xl text-coolGray-800">
@@ -22,6 +22,6 @@ export const InfoCard = ({ icon: Icon, title, children }: InfoCard) => {
 
 export const VerticalDivider = () => {
     return (
-        <div className="hidden lg:block w-0.5 border-r h-auto border-dashed border-gray-300" />
+        <div className="hidden xl:block w-0.5 border-r h-auto border-dashed border-gray-300" />
     );
 };
