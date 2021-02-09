31e0026df538b839c53ff8eaeadd8ae3948e7942
Esteban Garcia <estebangarcia2121@gmail.com>
2021-02-09 13:27:09 -0800

    Added link, image, and head mocks for nextjs

--- setup-tests.ts
+++ setup-tests.ts
@@ -1,1 +1,17 @@
 import "@testing-library/jest-dom"
+
+const children = () => ({ children }) => children
+
+jest.mock("next/link", () => children())
+jest.mock("next/head", () => children())
+
+process.env = {
+    ...process.env,
+    // @ts-ignore
+    __NEXT_IMAGE_OPTS: {
+        deviceSizes: [320, 420, 768, 1024, 1200],
+        imageSizes: [],
+        path: "/_next/image",
+        loader: "default"
+    }
+}
