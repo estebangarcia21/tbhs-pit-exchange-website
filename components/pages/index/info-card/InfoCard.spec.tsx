d3868f28005aeef59118c1773fe09e5bd8bb79a7
Esteban Garcia <estebangarcia2121@gmail.com>
2021-02-09 01:26:00 -0800

    Added InfoCard unit tests

---
+++ components/pages/index/info-card/InfoCard.spec.tsx
@@ -0,0 +1,41 @@
+import { render } from "@testing-library/react"
+import { BiDollarCircle } from "react-icons/bi"
+import { InfoCard } from "./InfoCard"
+
+describe("InfoCard", () => {
+    it("renders the title", () => {
+        const { getByText } = render(
+            <InfoCard title="Card Title" icon={[BiDollarCircle, "Aria Text"]}>
+                Hello World!
+            </InfoCard>
+        )
+
+        const title = getByText("Card Title")
+
+        expect(title).toBeInTheDocument()
+    })
+
+    it("renders the children", () => {
+        const { getByText } = render(
+            <InfoCard title="Card Title" icon={[BiDollarCircle, "Aria Text"]}>
+                Hello World!
+            </InfoCard>
+        )
+
+        const text = getByText("Hello World!")
+
+        expect(text).toBeInTheDocument()
+    })
+
+    it("renders icon properly", () => {
+        const { getAllByLabelText } = render(
+            <InfoCard title="Card Title" icon={[BiDollarCircle, "Aria Text"]}>
+                Hello World!
+            </InfoCard>
+        )
+
+        const icons = getAllByLabelText("Aria Text")
+
+        expect(icons.length).toBe(2)
+    })
+})
--- components/pages/index/info-card/InfoCard.tsx
+++ components/pages/index/info-card/InfoCard.tsx
@@ -1,16 +1,28 @@
-import { IconType } from "react-icons/lib"
+import { IconType } from "react-icons"
 import { Children } from "utils/children"
 
 interface InfoCard extends Children {
     title: string
-    icon: IconType
+    icon: [IconType, string]
 }
 
-export const InfoCard = ({ icon: Icon, title, children }: InfoCard) => {
+export const InfoCard = ({
+    icon: [Icon, ariaLabel],
+    title,
+    children,
+}: InfoCard) => {
     return (
         <div className="relative flex-grow md:m-5 xl:m-0 sm:flex-grow-0 sm:w-80 py-5">
-            <Icon className="text-3xl text-purpleBlue" />
-            <Icon className="absolute top-7 left-2 opacity-50 text-3xl text-teal-400" />
+            <Icon
+                className="text-3xl text-purpleBlue"
+                role="img"
+                aria-label={ariaLabel}
+            />
+            <Icon
+                className="absolute top-7 left-2 opacity-50 text-3xl text-teal-400"
+                role="img"
+                aria-label={ariaLabel}
+            />
 
             <h2 className="my-1 tracking-wide font-medium text-2xl text-coolGray-800">
                 {title}
