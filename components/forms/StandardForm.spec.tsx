93bc733d9ae3369ff8eae8ca3dbbac7e85cfc094
Esteban Garcia <estebangarcia2121@gmail.com>
2021-02-09 13:03:46 -0800

    Todo creates form inputs

--- components/forms/StandardForm.spec.tsx
+++ components/forms/StandardForm.spec.tsx
@@ -2,15 +2,5 @@ import { render } from "@testing-library/react"
 import { StandardForm } from "./StandardForm"
 
 describe("Standard Form", () => {
-    it("has leaves a form inpuit", () => {
-        const { getByLabelText } = render(
-            <StandardForm title="Test Form" handleSubmit={() => {}}>
-                Content
-            </StandardForm>
-        )
-
-        // const email = getByLabelText("email")
-
-        // expect(email).toBeInTheDocument()
-    })
+    it.todo("creates accessible form inputs")
 })
