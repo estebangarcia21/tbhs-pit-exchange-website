cba91c3c22e1efa8c422f4c5acdc6cb48f7995a8
Esteban Garcia <estebangarcia2121@gmail.com>
2021-02-09 13:40:19 -0800

    Added login page integration test

--- __tests__/login.spec.tsx
+++ __tests__/login.spec.tsx
@@ -1,11 +1,16 @@
-import { render, fireEvent } from "@testing-library/react"
-import { MockedProvider } from "@apollo/client/testing"
+import { MockedProvider, MockedResponse } from "@apollo/client/testing"
+import { fireEvent, render, waitFor } from "@testing-library/react"
+import {
+    LoginDocument,
+    LoginMutation,
+    LoginMutationVariables
+} from "generated/graphql-types"
 import Login from "pages/login"
 
 describe("login page", () => {
     it("has an email field", () => {
         const { getByLabelText } = render(
-            <MockedProvider mocks={[]}>
+            <MockedProvider>
                 <Login />
             </MockedProvider>
         )
@@ -17,7 +22,7 @@ describe("login page", () => {
 
     it("has a password field", () => {
         const { getByLabelText } = render(
-            <MockedProvider mocks={[]}>
+            <MockedProvider>
                 <Login />
             </MockedProvider>
         )
@@ -27,18 +32,103 @@ describe("login page", () => {
         expect(password).toBeInTheDocument()
     })
 
-    it("sends a login request on submit", () => {
-        // jest.mock("utils/server-client", () => ({
-        //     serverClient: {
-        //         request: jest.fn()
-        //     }
-        // }))
-        // const { getByText, getByLabelText } = render(<Login />)
-        // const email = getByLabelText(/email/i)
-        // const password = getByLabelText(/password/i)
-        // const submitButton = getByText(/login/i, { selector: "button" })
-        // fireEvent.change(email, { target: { value: "example@email.com" } })
-        // fireEvent.change(password, { target: { value: "123456" } })
-        // fireEvent.click(submitButton, { button: 1 })
+    it("redirects to the dashboard on successful login", async () => {
+        const mockedResponses: MockedResponse[] = [
+            {
+                request: {
+                    query: LoginDocument,
+                    variables: {
+                        email: "example@email.com",
+                        password: "123456"
+                    } as LoginMutationVariables
+                },
+                result: {
+                    data: {
+                        login: {
+                            successful: true
+                        }
+                    } as LoginMutation
+                }
+            }
+        ]
+
+        const useRouter = jest.spyOn(require("next/router"), "useRouter")
+        const routerPush = jest.fn()
+
+        useRouter.mockImplementation(() => ({
+            push: routerPush
+        }))
+
+        const { getByText, getByLabelText } = render(
+            <MockedProvider mocks={mockedResponses}>
+                <Login />
+            </MockedProvider>
+        )
+
+        const email = getByLabelText(/email/i)
+        const password = getByLabelText(/password/i)
+        const submitButton = getByText(/login/i, { selector: "button" })
+
+        fireEvent.change(email, { target: { value: "example@email.com" } })
+        fireEvent.change(password, { target: { value: "123456" } })
+        fireEvent.click(submitButton)
+
+        await waitFor(() => {
+            expect(routerPush).toHaveBeenCalledWith("/dashboard")
+        })
+    })
+
+    it("does not redirect to the dashboard on incorrect login and displays the error message", async () => {
+        const mockedResponses: MockedResponse[] = [
+            {
+                request: {
+                    query: LoginDocument,
+                    variables: {
+                        email: "example@email.com",
+                        password: "123456"
+                    } as LoginMutationVariables
+                },
+                result: {
+                    data: {
+                        login: {
+                            successful: false,
+                            error: {
+                                object: "email",
+                                message: "Incorrect email."
+                            }
+                        }
+                    } as LoginMutation
+                }
+            }
+        ]
+
+        const useRouter = jest.spyOn(require("next/router"), "useRouter")
+        const routerPush = jest.fn()
+
+        useRouter.mockImplementation(() => ({
+            push: routerPush
+        }))
+
+        const { getByText, getByLabelText } = render(
+            <MockedProvider mocks={mockedResponses}>
+                <Login />
+            </MockedProvider>
+        )
+
+        const email = getByLabelText(/email/i)
+        const password = getByLabelText(/password/i)
+        const submitButton = getByText(/login/i, { selector: "button" })
+
+        fireEvent.change(email, { target: { value: "example@email.com" } })
+        fireEvent.change(password, { target: { value: "123456" } })
+        fireEvent.click(submitButton)
+
+        await waitFor(() => {
+            expect(routerPush).not.toHaveBeenCalledWith("/dashboard")
+
+            const errorMessage = getByText(/incorrect email./i)
+
+            expect(errorMessage).toBeInTheDocument()
+        })
     })
 })
