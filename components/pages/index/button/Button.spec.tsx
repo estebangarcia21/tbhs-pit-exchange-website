4a661605852a4e516d21ce6068f434c7058f1f0f
Esteban Garcia <estebangarcia2121@gmail.com>
2021-02-09 08:50:26 -0800

    Added link test

--- components/pages/index/button/Button.spec.tsx
+++ components/pages/index/button/Button.spec.tsx
@@ -1,5 +1,26 @@
-import { render } from "@testing-library/react"
+import Router, { NextRouter } from "next/router"
+import { render, fireEvent } from "@testing-library/react"
 import { Button } from "./Button"
+
+export const mockRouter: NextRouter = {
+    basePath: "",
+    pathname: "/",
+    route: "/",
+    asPath: "/",
+    query: {},
+    push: jest.fn(),
+    replace: jest.fn(),
+    reload: jest.fn(),
+    back: jest.fn(),
+    prefetch: jest.fn().mockResolvedValue(undefined), // This one fixed it for me
+    beforePopState: jest.fn(),
+    events: {
+        on: jest.fn(),
+        off: jest.fn(),
+        emit: jest.fn(),
+    },
+    isFallback: false,
+}
 
 describe("Button", () => {
     it("renders a button with the child text", () => {
@@ -28,5 +49,17 @@ describe("Button", () => {
         const whiteButton = getByText("Hello World!")
 
         expect(whiteButton).toHaveClass("bg-white")
+    })
+
+    it("has the specified href", () => {
+        const { getByText } = render(
+            <Button href="/" white>
+                Hello World!
+            </Button>
+        )
+
+        const whiteButton = getByText("Hello World!", { selector: "a" })
+
+        expect(whiteButton).toHaveAttribute("href", "/")
     })
 })
