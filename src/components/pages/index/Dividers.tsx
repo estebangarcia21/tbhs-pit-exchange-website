0f60ce093dd757494c190090c32a9f4c8818b21c
Esteban Garcia <estebangarcia2121@gmail.com>
2021-02-09 22:21:53 -0800

    Made horizontal divider harder to see

--- src/components/pages/index/Dividers.tsx
+++ src/components/pages/index/Dividers.tsx
@@ -6,6 +6,6 @@ export const VerticalDivider = () => {
 
 export const HorizontalDivider = () => {
     return (
-        <div className="my-16 border-b border-dashed border-white border-opacity-20" />
+        <div className="my-16 border-b border-dashed border-gray-50 border-opacity-10" />
     )
 }
