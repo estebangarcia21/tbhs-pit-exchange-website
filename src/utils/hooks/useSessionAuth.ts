0d5a5d5417c8c36f85ab28e2b4af0e2f6cb5b3ad
Esteban Garcia <estebangarcia2121@gmail.com>
2021-02-09 17:55:49 -0800

    Used generated Apollo hooks

--- src/utils/hooks/useSessionAuth.ts
+++ src/utils/hooks/useSessionAuth.ts
@@ -1,23 +1,17 @@
-import { MeDocument, MeQuery, MeQueryVariables } from "generated/graphql-types"
+import { useMeQuery } from "generated/graphql-types"
 import { useRouter } from "next/router"
 import { useEffect } from "react"
-import { useQuery } from "react-query"
-import { serverClient } from "../server-client"
 
 export const useSessionAuth = () => {
     const router = useRouter()
 
-    const { data, isLoading } = useQuery(
-        "me",
-        async () =>
-            await serverClient.request<MeQuery, MeQueryVariables>(MeDocument)
-    )
+    const { data, loading } = useMeQuery()
 
     useEffect(() => {
-        if (!isLoading && !data?.me) {
-            router.replace("/login")
-        }
-    }, [isLoading, data, router])
+        if (loading || data) return
 
-    return { isLoading: isLoading && !data }
+        router.replace("/login")
+    }, [loading, data, router])
+
+    return { isLoading: loading && !data }
 }
