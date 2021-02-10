cfbeddb30e099fb6414a351ada8629b9c8dec302
Esteban Garcia <estebangarcia2121@gmail.com>
2021-02-09 19:54:30 -0800

    Completed index integration test

---
+++ __tests__/Index.spec.tsx
@@ -0,0 +1,64 @@
+import { render } from "@testing-library/react"
+import Index from "pages/index"
+
+describe("Index page", () => {
+    it("has the standard footer", () => {
+        const { getByTestId } = render(<Index />)
+
+        const footer = getByTestId("std-footer")
+
+        expect(footer).toBeInTheDocument()
+    })
+
+    describe("login button", () => {
+        it("exists", () => {
+            const { getByText } = render(<Index />)
+
+            const loginButton = getByText(/log in/i, { selector: "a" })
+
+            expect(loginButton).toBeInTheDocument()
+        })
+
+        it("redirects the user to the login page", () => {
+            const { getByText } = render(<Index />)
+
+            const loginButton = getByText(/log in/i, { selector: "a" })
+
+            expect(loginButton).toHaveAttribute("href", "/login")
+        })
+
+        it("is white", () => {
+            const { getByText } = render(<Index />)
+
+            const loginButton = getByText(/log in/i, { selector: "a" })
+
+            expect(loginButton).toHaveClass("bg-white")
+        })
+    })
+
+    describe("sign up button", () => {
+        it("exists", () => {
+            const { getByText } = render(<Index />)
+
+            const signupButton = getByText(/sign up/i, { selector: "a" })
+
+            expect(signupButton).toBeInTheDocument()
+        })
+
+        it("redirects the user to the sign up page", () => {
+            const { getByText } = render(<Index />)
+
+            const signupButton = getByText(/sign up/i, { selector: "a" })
+
+            expect(signupButton).toHaveAttribute("href", "/signup")
+        })
+
+        it("is teal", () => {
+            const { getByText } = render(<Index />)
+
+            const signupButton = getByText(/sign up/i, { selector: "a" })
+
+            expect(signupButton).toHaveClass("bg-teal-500")
+        })
+    })
+})
