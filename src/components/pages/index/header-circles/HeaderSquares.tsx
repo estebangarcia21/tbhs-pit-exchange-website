8be62c2f9b736d0b95975c3442c9f6765fda9ec3
Esteban Garcia <estebangarcia2121@gmail.com>
2021-02-09 22:22:06 -0800

    Set width to w-96

--- src/components/pages/index/header-circles/HeaderSquares.tsx
+++ src/components/pages/index/header-circles/HeaderSquares.tsx
@@ -6,7 +6,7 @@ export const HeaderSquares = ({ position }: HeaderImageProps) => {
     return (
         <img
             src="/images/index/squares.svg"
-            className={`absolute z-0 hidden lg:block w-80 ${position}`}
+            className={`absolute z-0 hidden lg:block w-96 ${position}`}
             alt="Squares in a grid pattern"
         />
     )
