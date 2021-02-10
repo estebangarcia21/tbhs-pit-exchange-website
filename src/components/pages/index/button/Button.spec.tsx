85079ee7cecd36799da1edb593265a36d680f415
Esteban Garcia <estebangarcia2121@gmail.com>
2021-02-09 19:54:46 -0800

    Removed unused mockRouter

--- src/components/pages/index/button/Button.spec.tsx
+++ src/components/pages/index/button/Button.spec.tsx
@@ -1,26 +1,5 @@
 import { render } from "@testing-library/react"
-import { NextRouter } from "next/router"
 import { Button } from "./Button"
-
-export const mockRouter: NextRouter = {
-    basePath: "",
-    pathname: "/",
-    route: "/",
-    asPath: "/",
-    query: {},
-    push: jest.fn(),
-    replace: jest.fn(),
-    reload: jest.fn(),
-    back: jest.fn(),
-    prefetch: jest.fn().mockResolvedValue(undefined), // This one fixed it for me
-    beforePopState: jest.fn(),
-    events: {
-        on: jest.fn(),
-        off: jest.fn(),
-        emit: jest.fn()
-    },
-    isFallback: false
-}
 
 describe("Button", () => {
     it("renders a button with the child text", () => {
