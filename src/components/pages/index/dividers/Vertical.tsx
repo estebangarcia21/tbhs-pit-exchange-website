ac9b59a31777716053ef018348d62d53380a5b0e
Esteban Garcia <estebangarcia2121@gmail.com>
2021-02-11 20:31:03 -0800

    Lightened up side border

--- src/components/pages/index/dividers/Vertical.tsx
+++ src/components/pages/index/dividers/Vertical.tsx
@@ -1,6 +1,6 @@
 const VerticalDivider = () => {
   return (
-    <div className="hidden xl:block w-0.5 border-l h-auto border-dashed border-gray-300" />
+    <div className="hidden xl:block w-0.5 border-l h-auto border-dashed border-coolGray-200" />
   )
 }
 
