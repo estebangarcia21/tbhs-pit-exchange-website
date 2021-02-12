8e58d3c850dc9b57fa553e9d4e40edfb1a3a6285
Esteban Garcia <estebangarcia2121@gmail.com>
2021-02-12 08:48:16 -0800

    Added minecraft item map

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require("tailwindcss/colors")
 
 module.exports = {
   purge: [
-    "./src/pages/**/*.tsx",
+    "./pages/**/*.tsx",
     "./src/components/**/*.tsx",
     "./src/styles/**/*.tsx"
   ],
