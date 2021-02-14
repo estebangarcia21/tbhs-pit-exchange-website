1650561717f190a9c3335107db4d9e91718b19fb
Esteban Garcia <estebangarcia2121@gmail.com>
2021-02-13 22:23:50 -0800

    Converted to twin macro

--- src/components/lib/Typography.tsx
+++ src/components/lib/Typography.tsx
@@ -1,9 +1,4 @@
-import Children from "utils/children"
+import tw from "twin.macro"
 
-export const Header = ({ children }: Children) => {
-  return <h1 className="text-3xl font-semibold text-grayBlue">{children}</h1>
-}
-
-export const Text = ({ children }: Children) => {
-  return <p className="my-2 leading-7 text-transparentBlue">{children}</p>
-}
+export const Header = tw.h1`text-3xl font-semibold text-grayBlue`
+export const Text = tw.p`my-2 leading-7 text-transparentBlue`
